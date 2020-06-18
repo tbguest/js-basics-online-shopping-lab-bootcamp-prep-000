7553b0cd8e0d2d4da7a1ed27338799ed33d74f47
Tristan Guest <tristanguest@gmail.com>
2020-06-18 12:32:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function viewCart() {
     if (cart.length === 0) {
       return "Your shopping cart is empty."
     } else if (cart.length === 1) {
-      
+      inCart = `${inCart} ${cart[i].itemName} at \$${cart[i].itemPrice}.`
     } else if (i === parseInt(cart.length) - 1) {
       inCart = `${inCart} and ${cart[i].itemName} at \$${cart[i].itemPrice}.`
     } else {
